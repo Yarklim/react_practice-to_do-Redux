65eb0e4ce4632be75e07183e87cc4a06249940fa
Yaroslav Klimas <yarklim@gmail.com>
2023-10-14 13:39:25 +0300

    update tasksList

--- src/components/TasksFilter/TasksFilter.jsx
+++ src/components/TasksFilter/TasksFilter.jsx
@@ -1,14 +1,39 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { getStatusFilter } from '../../redux/selectors';
+import { setStatusFilter } from '../../redux/actions';
+import { statusFilters } from '../../redux/constants';
 import s from './TasksFilter.module.scss';
 
 const TasksFilter = () => {
+  const dispatch = useDispatch();
+
+  const filter = useSelector(getStatusFilter);
+
+  const handleFilterChange = (filter) => dispatch(setStatusFilter(filter));
+
   return (
     <div className={s.filterWrapper}>
       <h2>Filter by status</h2>
       <div className={s.buttonWrapper}>
-        <button>All</button>
-        <button>Active</button>
-        <button>Complited</button>
+        <button
+          selected={filter === statusFilters.all}
+          onClick={() => handleFilterChange(statusFilters.all)}
+        >
+          All
+        </button>
+        <button
+          selected={filter === statusFilters.active}
+          onClick={() => handleFilterChange(statusFilters.active)}
+        >
+          Active
+        </button>
+        <button
+          selected={filter === statusFilters.completed}
+          onClick={() => handleFilterChange(statusFilters.completed)}
+        >
+          Complited
+        </button>
       </div>
     </div>
   );
